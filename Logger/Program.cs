8c63937c99a9c7e1138d1f227a847a3bb6d8f794
Mathias Iden <mathias.iden@dnb.no>
2023-04-04 12:55:52 +0200

    Add dependency injection

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -1,3 +1,6 @@
+using Logger.Repository;
+using Logger.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register a single instance of the repository. Same instance will be used for every request
+builder.Services.AddSingleton<ISbankenEventRepository, SbankenEventRepository>();
+
+// A new instance of event service will be created for each request
+builder.Services.AddScoped<IEventService, EventService>();
 
 var app = builder.Build();
 
---
+++ Logger/Repository/ISbankenEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using Logger.Models;
+
+namespace Logger.Repository
+{
+    public interface ISbankenEventRepository
+    {
+        void Delete(string eventId);
+        SbankenEvent[] GetAll();
+        SbankenEvent GetById(string eventId);
+        void Save(SbankenEvent sbankenEvent);
+    }
+}
